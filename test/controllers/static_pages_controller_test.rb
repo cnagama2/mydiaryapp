17cdf24294b5b77cb2e9b8c95c6a279f1854e652
cnagama2 <chikako_mkthrk@yahoo.co.jp>
2018-12-28 01:07:35 +0000

    リンクのテスト

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,19 +2,10 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
-  def setup
-    @base_title = "My Diary"
-  end 
-  
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-  
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select "title", "My Diary"
   end
 
 end
